ba54bd68461cf3a0c5cb9a45cef89fa815c8e766
khalil krifi <88883927+DaL1ght1@users.noreply.github.com>
2024-01-28 14:55:43 +0100

    Update NavBar.jsx

--- src/navbar/NavBar.jsx
+++ src/navbar/NavBar.jsx
@@ -5,25 +5,32 @@ import "./navbar.css";
 export default function Navbar() {
   return (
     <>
-      <a href="/">
-        <img
-          src={logo}
-          alt="Company Logo"
-          className="logo"
-          width="200"
-          height="100"
-        />
-      </a>
+      <div className="backnav">
+        <div className="header">
+          <a href="/">
+            <img
+              src={logo}
+              alt="Company Logo"
+              className="logo"
+              width="200"
+              height="100"
+            />
+          </a>
 
-      <nav className="navbar">
-        <div className="navigation">
-          <NavLink to="/home">Home</NavLink>
-          <NavLink to="/program">Program</NavLink>
-          <NavLink to="/aboutus">About Us</NavLink>
-          <NavLink to="/contactus">Contact Us</NavLink>
-          <NavLink to="/registration">Registration</NavLink>
+          <nav className="navbar">
+            <div className="navigation">
+              <NavLink to="/home">Home</NavLink>
+              <NavLink to="/program">Program</NavLink>
+              <NavLink to="/aboutus">About Us</NavLink>
+              <NavLink to="/contactus">Contact Us</NavLink>
+
+              <NavLink className="registred" to="/registration">
+                Registration
+              </NavLink>
+            </div>
+          </nav>
         </div>
-      </nav>
+      </div>
     </>
   );
 }
