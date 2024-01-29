c74dfbb3bf2504150a4134279c179c5a2fb6c888
khalil krifi <88883927+DaL1ght1@users.noreply.github.com>
2024-01-29 01:32:31 +0100

    Update NavBar.jsx

--- src/navbar/NavBar.jsx
+++ src/navbar/NavBar.jsx
@@ -1,13 +1,18 @@
+import React from "react";
 import { NavLink } from "react-router-dom";
-import logo from "/src/components/logo.png";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import "./navbar.css";
+import logo from "/src/components/logo.png";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
-export default function Navbar() {
+function CollapsibleExample() {
   return (
-    <>
-      <div className="backnav">
-        <div className="header">
-          <a href="/">
+    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">
+          <a>
             <img
               src={logo}
               alt="Company Logo"
@@ -16,21 +21,53 @@ export default function Navbar() {
               height="100"
             />
           </a>
-
-          <nav className="navbar">
-            <div className="navigation">
-              <NavLink to="/home">Home</NavLink>
-              <NavLink to="/program">Program</NavLink>
-              <NavLink to="/aboutus">About Us</NavLink>
-              <NavLink to="/contactus">Contact Us</NavLink>
-
-              <NavLink className="registred" to="/registration">
-                Registration
-              </NavLink>
-            </div>
-          </nav>
-        </div>
-      </div>
-    </>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <NavLink to="/home" className="nav-link" activeClassName="active">
+              Home
+            </NavLink>
+            <NavLink
+              to="/program"
+              className="nav-link"
+              activeClassName="active"
+            >
+              Program
+            </NavLink>
+            <NavLink
+              to="/aboutus"
+              className="nav-link"
+              activeClassName="active"
+            >
+              About Us
+            </NavLink>
+            <NavLink
+              to="/contactus"
+              className="nav-link"
+              activeClassName="active"
+            >
+              Contact Us
+            </NavLink>
+          </Nav>
+          <Nav>
+            <NavLink
+              to="/registration"
+              className="nav-link"
+              activeClassName="active"
+              style={{
+                backgroundColor: "#1d202b",
+                color: "#feda5e",
+                borderRadius: "5rem",
+              }}
+            >
+              Registration
+            </NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
+
+export default CollapsibleExample;
